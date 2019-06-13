fdb06925a9f67904a64dd2ddfde74fc356ff8ac1
ROSNET Gilian <TheScottishFly@users.noreply.github.com>
2019-06-13 09:52:21 +0200

    add analytics

--- src/main.js
+++ src/main.js
@@ -3,14 +3,20 @@ import VueTypedJs from 'vue-typed-js';
 import '@/assets/css/base.css';
 import '@/assets/css/tailwind.min.css';
 import App from './App.vue';
-import router from './router'
-import store from './store/index'
+import router from './router';
+import store from './store/index';
+import VueAnalytics from 'vue-analytics';
 
 require('vue2-animate/dist/vue2-animate.min.css');
 
 Vue.config.productionTip = false;
 
 Vue.use(VueTypedJs);
+
+Vue.use(VueAnalytics, {
+  id: 'UA-142050268-2',
+  router
+});
 
 new Vue({
   render: h => h(App),
